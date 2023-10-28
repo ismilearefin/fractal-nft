0a9fb9a0fe69002543e90a3f0e542608cdb90a23
ismilearefin <arefinismile@gmail.com>
2023-10-28 20:04:55 +0600

    Fractal section Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
-// import Fractal from "./components/FractalSection/Fractal";
+import Fractal from "./components/FractalSection/Fractal";
 // import BackStory from "./components/BackStory/BackStory";
 // import RoadMap from "./components/RoadMap/RoadMap";
 // import TeamSection from "./components/Team/TeamSection";
@@ -17,7 +17,7 @@ function App() {
       <div className="customWidth mx-auto">
         <Navbar></Navbar>
         <HeroSection></HeroSection>
-        {/* <Fractal></Fractal> */}
+        <Fractal></Fractal>
         {/* <BackStory></BackStory> */}
         {/* <RoadMap> </RoadMap> */}
         {/* <TeamSection></TeamSection> */}
--- src/components/FractalSection/Fractal.jsx
+++ src/components/FractalSection/Fractal.jsx
@@ -19,10 +19,10 @@ const data = [
 
 export default function Fractal() {
   return (
-    <div className="d-flex gap-5 justify-content-evenly border-bottom py-5">
+    <div className="d-flex gap-5 justify-content-evenly border-bottom py-3 py-md-5">
       {
         data.map(item=>(
-            <div key={item.id} className="text-white d-flex align-items-center gap-4">
+            <div key={item.id} className="d-none d-md-flex text-white  align-items-center gap-4">
             <svg
         xmlns="http://www.w3.org/2000/svg"
         width="47"
@@ -39,6 +39,21 @@ export default function Fractal() {
       </div>
         ))
       }
+      <div className="text-center text-white d-flex align-items-center justify-content-center gap-4">
+            <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="47"
+        height="46"
+        viewBox="0 0 47 46"
+        fill="none"
+      >
+        <path
+          d="M23.3743 0.0930176L46.2812 23L23.3743 45.907L0.467285 23L23.3743 0.0930176Z"
+          fill="white"
+        />
+      </svg>
+      <h1>Fractal</h1>
+      </div>
     </div>
   );
 }
